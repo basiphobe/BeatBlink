dda88ee0cbbda459e07ac37471be5c58a0bcb64e
basiphobe <basiphobe@gmail.com>
2025-11-01 20:47:43 -0400

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.beatblink"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
